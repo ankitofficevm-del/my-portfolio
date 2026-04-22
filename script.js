a7619a4fef654c9449e7e458e6f075b9500c8b14
Ankit Chauhan <ankitofficevm@gmail.com>
2026-04-22 10:42:21 +0530

    script.js

--- script.js
+++ script.js
@@ -418,3 +418,26 @@ scrollBtn.addEventListener("click", () => {
     behavior: "smooth" // Smooth scrolling effect
   });
 });
+
+
+// Right click disable karne ke liye
+    document.addEventListener('contextmenu', event => event.preventDefault());
+
+    // Keyboard shortcuts disable karne ke liye
+    document.onkeydown = function(e) {
+        if (e.keyCode == 123) { // F12
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+            return false;
+        }
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+            return false;
+        }
+        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+            return false;
+        }
+    };
